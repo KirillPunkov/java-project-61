a238db3bc46f2d13a149523d5eda6ad973231587
Kirill Pankov <tiur@list.ru>
2023-04-04 05:21:50 +1000

    Update Even.java

--- app/src/main/java/hexlet/code/Even.java
+++ app/src/main/java/hexlet/code/Even.java
@@ -7,13 +7,15 @@ public class Even {
     static Scanner scanner = new Scanner(System.in);
     static Random random = new Random();
 
+    private static String playerName = Greet.getPlayerName();
+
     //private static int countOfWrightAnswers = 0;
     public static void gameEven() {
         Greet.greetPlayer();
 
         System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'");
 
-        for (var i = 0; i <= 3; i++) {
+        for (var i = 0; i < 3; i++) {
             int randomNumber = random.nextInt();
             boolean isRandomNumberEven = ((randomNumber % 2) == 0);
             System.out.println("Question: " + randomNumber);
@@ -21,25 +23,28 @@ public class Even {
             String playerAnswer = scanner.next();
             playerAnswer = playerAnswer.toLowerCase();
 
-            switch (isRandomNumberEven) {
-                case true:
-                   if (playerAnswer.equals("yes")) {
-                       System.out.println("Correct!");
-                   } else {
-                       System.out.println(playerAnswer + " is wrong answer ;(. Correct answer was 'yes'.");
-                   }
-                   break;
-                case false:
-                    if (playerAnswer.equals("no")) {
-                        System.out.println("Correct!");
-                    } else {
-                        System.out.println(playerAnswer + " is wrong answer ;(. Correct answer was 'no'.");
-                    }
+            if (isRandomNumberEven) {
+                if (playerAnswer.equals("yes")) {
+                    System.out.println("Correct!");
+                } else {
+                    System.out.println(playerAnswer + " is wrong answer ;(. Correct answer was 'yes'.");
+                    wrongEndOfGame();
                     break;
-                default:
+                }
+            } else {
+                if (playerAnswer.equals("no")) {
+                    System.out.println("Correct!");
+                } else {
+                    System.out.println(playerAnswer + " is wrong answer ;(. Correct answer was 'no'.");
+                    wrongEndOfGame();
                     break;
+                }
             }
         }
-        System.out.println("Congratulations, " + Greet.getPlayerName());
+        System.out.println("Congratulations, " + playerName + "!");
+    }
+
+    private static void wrongEndOfGame() {
+        System.out.println("Let's try again, " + playerName + "!");
     }
 }
